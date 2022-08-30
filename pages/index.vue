5bc8da98f206db9a8daa4c89e7d51fccf3725fde
Ismet1999 <kadyrovismet1999@gmail.com>
2022-08-31 02:17:09 +0500

    add home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,51 @@
 <template>
-  <div></div>
+  <div>
+    <div class="p-2">
+      <BFormInput placeholder="Enter a book title" />
+    </div>
+
+    <div class="p-2">
+      <BCard no-body>
+        <BTable
+          responsive
+          striped
+          hover
+          :items="items"
+          :fields="fields"
+        ></BTable>
+      </BCard>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  data() {
+    return {
+      items: [
+        {
+          id: 1,
+          title: 'Harry Potter',
+          author: 'J.K. Rowling',
+          year: '1997',
+        },
+      ],
+      fields: [
+        {
+          key: 'image',
+          label: 'Image',
+        },
+        {
+          key: 'title',
+          label: 'Title',
+        },
+        {
+          key: 'author',
+          label: 'Author',
+        },
+      ],
+    }
+  },
+}
+</script>
